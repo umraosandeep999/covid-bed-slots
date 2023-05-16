f4105979cd68dc4bc6d174a6f8ba434efa383947
umraosandeep999 <126574065+umraosandeep999@users.noreply.github.com>
2023-05-16 22:07:06 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+# with open('config.json','r') as c:
+#     params=json.load(c)["params"]
+
+
+
+# app.config.update(
+#     MAIL_SERVER='smtp.gmail.com',
+#     MAIL_PORT='465',
+#     MAIL_USE_SSL=True,
+#     MAIL_USERNAME='gmail account',
+#     MAIL_PASSWORD='gmail account password'
+# # )
